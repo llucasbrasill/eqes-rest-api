4e73b72a7ba4a6b3f593048d2381a602bb9cc857
Lucas P. Sodré <llucasbrasill@gmail.com>
2021-05-02 12:09:47 -0300

    :heavy_plus_sign: fix url server

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const port = process.env.SERVER_PORT;
 app.listen(port, () => {
   console.log();
   console.log(`Listen the port: ${port}`);
-  console.log(`Application url: http://localhost/${port}`);
+  console.log(`Application url: http://localhost:${port}`);
   console.log();
 });
